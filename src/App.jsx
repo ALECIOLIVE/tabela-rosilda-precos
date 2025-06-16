51113b88214c084ef11867e227235d782fede805
ALECIOLIVE <aleciolive@gmail.com>
2025-06-15 23:43:23 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+
+import React, { useEffect, useState } from 'react';
+
 function App() {
-  return <h1>Aplicativo de Tabela de Preços</h1>
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    fetch('/src/assets/tabelas_precos.json')
+      .then(response => response.json())
+      .then(data => setDados(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Tabela de Preços</h1>
+      <ul>
+        {dados.map((item, index) => (
+          <li key={index}>{item.tipo} - {item.servico} - R${item.preco}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
